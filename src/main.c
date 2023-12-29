6d4f8aaa0729fc90879e7f77f2cb9e2c22a50cd5
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-12-29 01:38:05 +0100

    Map keys, limit framerate -- try to :), init c8_renderer

--- src/main.c
+++ src/main.c
@@ -8,17 +8,30 @@
 #define WINDOW_WIDTH  64*PIXEL_SCALE
 #define WINDOW_HEIGHT 32*PIXEL_SCALE
 
-void pixel_renderer(int pixel, int x, int y, void **userData) {
+#define _EXTRACT_RENDERER_FROM_USER_DATA(userData)  SDL_Renderer *renderer = (SDL_Renderer*)(*userData)
+
+void pixel_clear(void **userData) {
+    _EXTRACT_RENDERER_FROM_USER_DATA(userData);
+
+    SDL_RenderClear(renderer);
+}
+
+void pixel_render(void **userData) {
+    _EXTRACT_RENDERER_FROM_USER_DATA(userData);
+
+    SDL_RenderPresent(renderer);
+}
+
+void pixel_set(int pixel, int x, int y, void **userData) {
     int color;
-    SDL_Renderer *ren;
+
+    _EXTRACT_RENDERER_FROM_USER_DATA(userData);
 
     color = (pixel == 0 ? 0x00 : 0xFF); 
-    ren = (SDL_Renderer*)(*userData);
 
-    SDL_SetRenderDrawColor(ren, color, color, color, 0xFF);
-
-    SDL_RenderDrawPoint(ren, x, y);
-    SDL_RenderPresent(ren);
+    SDL_SetRenderDrawColor(renderer, color, color, color, 0xFF);
+    SDL_RenderDrawPoint(renderer, x, y);
+    
 }
 
 int load_prgm(struct chip8 *context, int argc, char **argv) {
@@ -40,9 +53,11 @@ int main(int argc, char** argv) {
     int bRunning;
     struct chip8 _context;
     struct chip8 *context;
+    struct c8_renderer c8Renderer;
     WORD opcode;
     SDL_Window *win;
     SDL_Renderer *ren;
+    Uint64 prevTicks, ticks, delta;
 
     /* init components */
     bRunning = 1;
@@ -70,7 +85,12 @@ int main(int argc, char** argv) {
 
     SDL_RenderSetScale(ren, PIXEL_SCALE, PIXEL_SCALE);
 
-    c8_reset(context, pixel_renderer, (void*)&ren);
+    c8Renderer.clear = pixel_clear;
+    c8Renderer.render = pixel_render;
+    c8Renderer.set_pixel = pixel_set;
+    c8Renderer.userData = (void*)&ren;
+
+    c8_reset(context, &c8Renderer);
     int rv = load_prgm(context, argc, argv);
     if (rv != 0 ) {
         c8_destroy(context);
@@ -81,36 +101,34 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    for (int i = USER_MEMORY_START; i < USER_MEMORY_END; i+=2) {
-        if (context->memory[i] == 0 && context->memory[i+1] == 0) {
-            break;
-        }
-        
-        printf("0x%02hhx%02hhx\n", context->memory[i], context->memory[i+1]);
-    }
-
-    int tmp = 0;
     while(bRunning) {
         SDL_Event event;
+        ticks = SDL_GetTicks64();
+        delta = ticks - prevTicks;
 
         while(SDL_PollEvent(&event)) {
             if( (SDL_QUIT == event.type) || 
                 (SDL_KEYDOWN == event.type && SDLK_ESCAPE == event.key.keysym.sym) ) {
                 bRunning = 0;
                 break;
+            } else if (SDL_KEYDOWN == event.type) {
+                int key = event.key.keysym.sym;
+
+                if (key >= 0x30 && key <= 0x39)                 // 0 to 9
+                    context->keyPressed = key & 0x0F;
+                else if (key >= 0x60 && key <= 0x65)            // A to F
+                    context->keyPressed = (key & 0x0F) + 0x9;
+            } else if (SDL_KEYUP == event.type) {
+                context->keyPressed = -1;
             }
         }
 
-        context->keyPressed = (SDL_KEYDOWN == event.type ? event.key.keysym.sym : -1);
+        if (delta > (1000/60.0)) {
+            if (c8_tick(context) != 0) {
+                break;
+            }
 
-        WORD opcode;
-        opcode = c8_fetch(context);
-        c8_decode(context, opcode);
-
-        c8_error_t err;
-        if ((err = c8_get_error(context)).err != C8_GOOD) {
-            printf("c8_decode Error(%d): %s at %04x(%04x)\n", err.err, err.msg, context->pc, opcode);
-            break;
+            prevTicks = ticks;
         }
     }
 
